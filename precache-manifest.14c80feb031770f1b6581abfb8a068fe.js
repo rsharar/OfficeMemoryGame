8aa6590e4e021f15dcea114ff0f17f1716de1acd
Rudy Sharar <rudy@activelylearn.com>
2019-01-10 21:09:16 -0800

    Updates

---
+++ precache-manifest.14c80feb031770f1b6581abfb8a068fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5feccc65fec5fa08402",
+    "url": "/OfficeMemoryGame/static/js/runtime~main.e5feccc6.js"
+  },
+  {
+    "revision": "84f2845d16179c107af1",
+    "url": "/OfficeMemoryGame/static/js/main.84f2845d.chunk.js"
+  },
+  {
+    "revision": "b05f58ae460c5f47cea3",
+    "url": "/OfficeMemoryGame/static/js/1.b05f58ae.chunk.js"
+  },
+  {
+    "revision": "84f2845d16179c107af1",
+    "url": "/OfficeMemoryGame/static/css/main.26b4e4b8.chunk.css"
+  },
+  {
+    "revision": "b6a05f03b9401d492ebe7c03b1686aa7",
+    "url": "/OfficeMemoryGame/index.html"
+  }
+];
